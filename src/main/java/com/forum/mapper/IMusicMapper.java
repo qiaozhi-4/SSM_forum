f8eb1c7a3a68bb1717223bf4e9858bbf86836aae
13135276414 <207752652@qq.com>
2022-05-23 16:03:06 +0800

    111

--- src/main/java/com/forum/mapper/IMusicMapper.java
+++ src/main/java/com/forum/mapper/IMusicMapper.java
@@ -3,7 +3,12 @@ package com.forum.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.forum.entity.Music;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface IMusicMapper extends BaseMapper<Music> {
+    List<Music> findByUserId(@Param("id") int id, @Param("name") String name);
+
 }
--- src/main/java/com/forum/mapper/IUserMapper.java
+++ src/main/java/com/forum/mapper/IUserMapper.java
@@ -1,9 +1,15 @@
 package com.forum.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.forum.entity.Music;
 import com.forum.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface IUserMapper extends BaseMapper<User> {
+    List<Music> findByUserIdSelectAttention(@Param("id") int id);
+    List<Music> findByUserIdSelectFans(@Param("id") int id);
 }
