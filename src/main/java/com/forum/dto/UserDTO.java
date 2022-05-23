865c76c5940d29df371b0b8646821da81192b3ae
“18890176107” <18890176107@163.com>
2022-05-23 14:39:14 +0800

    ssm-forum

---
+++ src/main/java/com/forum/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package com.forum.dto;
+
+import com.forum.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+这是一个DTO，只需要保留合适的字段即可
+使用JSON时发送到前端
+ */
+@Data @AllArgsConstructor @NoArgsConstructor
+public class UserDTO
+{
+    private int id;
+    private String username;
+    private double money;
+    
+    // User转UserDTO的静态【工厂】方法
+    public static UserDTO fromUser(User user)
+    {
+        return new UserDTO(user.getId(), user.getUsername(), user.getMoney());
+    }
+}
--- src/main/java/com/forum/service/IUserService.java
+++ src/main/java/com/forum/service/IUserService.java
@@ -4,4 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.forum.entity.User;
 
 public interface IUserService extends IService<User> {
+
+    //注册
+    int register (String username, String password1, String password2);
 }
