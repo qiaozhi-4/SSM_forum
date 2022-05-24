bd89f65b4c4bbc19b8659c53cf12c95c885ed484
“18890176107” <18890176107@163.com>
2022-05-24 16:27:36 +0800

    ssm-forum

--- src/main/java/com/forum/controller/MusicController.java
+++ src/main/java/com/forum/controller/MusicController.java
@@ -2,18 +2,12 @@ package com.forum.controller;
 
 import com.alibaba.fastjson2.JSON;
 import com.forum.entity.Music;
-import com.forum.entity.User;
 import com.forum.service.IMusicService;
-import com.forum.service.IUserService;
 import lombok.RequiredArgsConstructor;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.*;
-import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
@@ -24,16 +18,15 @@ import java.util.List;
 @RequiredArgsConstructor
 //开启事务管理
 @EnableTransactionManagement
-public class MUsicController {
+public class MusicController {
 
     private final IMusicService musicService;
     private final JedisPool pool;
 
 
-
     //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 歌单 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     @RequestMapping(value = "/Playlist")
-    public String register(String id, String name,String pageNum,Model model) {
+    public String register(String id, String name, String pageNum, Model model) {
         //redis
         try (Jedis jedis = pool.getResource()) {
             //先查redis有没有
@@ -49,6 +42,17 @@ public class MUsicController {
             model.addAttribute("musics", musics);
         }
         return "Playlist";
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 模糊搜索 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @RequestMapping(value = "/fuzzy")
+    public String fuzzy(String str, String pageNum, Model model) {
+        int page = 1;
+        if (pageNum != null){
+            page = Integer.parseInt(pageNum);
+        }
+        List<Music> music = musicService.findByFuzzy(str, page);
+        return "";
     }
 
 
