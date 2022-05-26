ea75f71b82d696712d9ff80731dd803ab332d5c4
“18890176107” <18890176107@163.com>
2022-05-26 17:26:14 +0800

    ssm-forum

--- src/main/java/com/forum/config/ServiceLogAspect.java
+++ src/main/java/com/forum/config/ServiceLogAspect.java
@@ -18,23 +18,25 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 //定义切面类
 public class ServiceLogAspect {
     private Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
-    private File file =new File("log.txt");
 
 
 
     //定义通知，方法执行前
     public void doBefore(JoinPoint poin) throws IOException {
-        logger.info("【Service】方法执行前,当前时间："+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        System.out.println("------------------------------------------------------");
+        System.out.println("【Service】方法执行前,当前时间："+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        System.out.println("------------------------------------------------------");
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
         // 记录下请求内容
+        logger.info("【Service】方法执行前,当前时间："+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         logger.info("【Service】请求URL : " + request.getRequestURL().toString());
         logger.info("【Service】请求方法 : " + request.getMethod());
         logger.info("【Service】IP地址 : " + request.getRemoteAddr());
         Enumeration<String> enu = request.getParameterNames();
 
-
-        FileWriter fileWritter = new FileWriter(file.getName(),true);
+        File file =new File("log.txt");
+        FileWriter fileWritter = new FileWriter("log.txt",true);
         BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
         bufferWritter.write("【Service】方法执行前,当前时间："+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         bufferWritter.write("【Service】请求URL : " + request.getRequestURL().toString());
@@ -51,6 +53,7 @@ public class ServiceLogAspect {
 
     //定义通知，方法执行后
     public void after(JoinPoint poin) throws IOException {
+        File file =new File("log.txt");
         FileWriter fileWritter = new FileWriter(file.getName(),true);
         BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
         bufferWritter.write("【Service】方法执行后,当前时间："+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
@@ -61,6 +64,7 @@ public class ServiceLogAspect {
 
     //定义通知，方法返回前
     public void AfterReturning(JoinPoint poin) throws IOException {
+        File file =new File("log.txt");
         FileWriter fileWritter = new FileWriter(file.getName(),true);
         BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
         bufferWritter.write("【Service】方法返回前,当前时间："+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
@@ -70,6 +74,7 @@ public class ServiceLogAspect {
 
     //定义通知，抛出异常
     public void AfterThrowing(Throwable error) throws IOException {
+        File file =new File("log.txt");
         FileWriter fileWritter = new FileWriter(file.getName(),true);
         BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
         bufferWritter.write("【Service】方法报错,当前时间："+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
@@ -79,6 +84,7 @@ public class ServiceLogAspect {
 
     //定义通知环绕型
     public Object Around (ProceedingJoinPoint pjp) throws Throwable{
+        File file =new File("log.txt");
         FileWriter fileWritter = new FileWriter(file.getName(),true);
         BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
         bufferWritter.write("【Service】环绕前:"+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
